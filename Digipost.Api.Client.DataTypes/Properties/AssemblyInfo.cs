8f6044e30a365ed0aba7c47a7655830438e88793
AaronArrick <arrickaaron@gmail.com>
2019-11-21 13:29:34 +0100

    Flytt DataTypes fra digipost-api-client-dotnet hit.

---
+++ Digipost.Api.Client.DataTypes/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Digipost.Api.Client.DataTypes")]
+[assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("Digipost.Api.Client.DataTypes.Tests")]
