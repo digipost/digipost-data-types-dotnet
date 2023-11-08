1b203d13275ef341dd5338677ceea1700b6dbe77
Eivind Bergstøl <eivind@bergstol.no>
2023-11-08 13:15:50 +0100

    Started work on abstracting the domain for dotnet data types
    We want to separate the xml-generated domain-classes away from the
    domain used in the dot net client. This will make us able to
    serialize the classes as we see fit in stead of pushing
    serialisation logic to the programmer using the dotnet client.

---
+++ Digipost.Api.Client.DataTypes.Core/Internal/SerializeUtil.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Digipost.Api.Client.DataTypes.Core.Internal
+{
+    public static class SerializeUtil
+    {
+        public static string Serialize<T>(T value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("", "http://api.digipost.no/schema/datatypes");
+
+            var serializer = new XmlSerializer(value.GetType());
+
+            var settings = new XmlWriterSettings
+            {
+                Encoding = new UTF8Encoding(false),
+                ConformanceLevel = ConformanceLevel.Document,
+                Indent = false,
+                OmitXmlDeclaration = true
+            };
+
+            using (var textWriter = new Utf8StringWriter())
+            {
+                using (var xmlWriter = XmlWriter.Create(textWriter, settings))
+                {
+                    serializer.Serialize(xmlWriter, value, ns);
+                }
+
+                return textWriter.ToString();
+            }
+        }
+
+        private sealed class Utf8StringWriter : StringWriter
+        {
+            public override Encoding Encoding => Encoding.UTF8;
+        }
+    }
+}
--- Digipost.Api.Client.DataTypes.Utils/GenerationUtil.cs
+++ Digipost.Api.Client.DataTypes.Utils/GenerationUtil.cs
@@ -11,7 +11,7 @@ namespace Digipost.Api.Client.DataTypes.Utils
             // Build folder:
             // digipost-data-types-dotnet/Digipost.Api.Client.DataTypes.Utils/bin/Debug/netcoreapp3.1/Digipost.Api.Client.DataTypes.Utils.dll
             var projectRootRelativePath = "../../../..";
-            
+
             Generator generator = new Generator
             {
                 OutputFolder = $"{projectRootRelativePath}/Digipost.Api.Client.DataTypes.Core/",
@@ -19,11 +19,11 @@ namespace Digipost.Api.Client.DataTypes.Utils
                 GenerateNullables = true,
                 DisableComments = true,
                 GenerateInterfaces = true,
-                OutputWriter = new SingleFileOutputWriter($"{projectRootRelativePath}/Digipost.Api.Client.DataTypes.Core/") { OutputFile = "DataTypes.cs"},
+                OutputWriter = new SingleFileOutputWriter($"{projectRootRelativePath}/Digipost.Api.Client.DataTypes.Core/Internal") { OutputFile = "DataTypes.cs"},
                 GenerateComplexTypesForCollections = true,
                 NamespaceProvider = new Dictionary<NamespaceKey, string>
                 {
-                    { new NamespaceKey("http://api.digipost.no/schema/datatypes"), "Digipost.Api.Client.DataTypes.Core" }
+                    { new NamespaceKey("http://api.digipost.no/schema/datatypes"), "Digipost.Api.Client.DataTypes.Core.Internal" }
                 }.ToNamespaceProvider(new GeneratorConfiguration{ NamespacePrefix = "DataTypes" }.NamespaceProvider.GenerateNamespace)
             };
 
